4dd381f8f3b18e182d45dfced0d79cacecfc4110
nanatila <nanatila@gmail.com>
2021-11-16 07:41:58 -0500

    splash

--- app/src/main/java/com/example/estampamos/MainActivity.kt
+++ app/src/main/java/com/example/estampamos/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.estampamos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.concurrent.thread
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        setTheme(R.style.Theme_Estampamos)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
