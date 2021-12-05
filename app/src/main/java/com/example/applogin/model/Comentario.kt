89aa306f2f271deb51a0c19b735d8a7e63397e8c
nanatila <nanatila@gmail.com>
2021-12-05 11:31:07 -0500

    comentarios

---
+++ app/src/main/java/com/example/applogin/model/Comentario.kt
@@ -0,0 +1,9 @@
+package com.example.applogin.model
+
+class Comentario (
+
+    var nombreUsario:String,
+    var puntaje:Int,
+    var texto:String
+
+        )
--- app/src/main/java/com/example/applogin/view/ui/fragments/CommentsFragment.kt
+++ app/src/main/java/com/example/applogin/view/ui/fragments/CommentsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.applogin.R
+import com.example.applogin.databinding.FragmentCommentsBinding
+import com.example.applogin.model.Comentario
+import com.example.applogin.view.adapter.ComentarioAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,13 +18,29 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ComentsFragment.newInstance] factory method to
+ * Use the [CommentsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ComentsFragment : Fragment() {
+class CommentsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    var comentarios = listOf(
+
+        Comentario("Pilar",5, "La mejor Tienda del mundo"),
+        Comentario("Francisco", 4,"Recomendada"),
+        Comentario("Harold",5, "Le doy 5 estrellas"),
+        Comentario("Andres",3, "Las mejores Camisetas"),
+        Comentario("Adriana",5, "Excelente Aplicación"),
+        Comentario("Elena",4, "Buena"),
+        Comentario("Sergio",5, "Compramos con seguridad"),
+        Comentario("Mauricio",4, "Me gusta esta aplicación"),
+        Comentario("Maria",5, "Me gustan sus diseños"),
+        Comentario("Esteban",5, "Es muy divertida")
+
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,14 +48,40 @@ class ComentsFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coments, container, false)
+        return inflater.inflate(R.layout.fragment_comments, container, false)
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+        //var rvComments = view.findViewById<RecyclerView>(R.id.rvComments)
+
+        val binding = FragmentCommentsBinding.bind(view)
+
+        binding.rvComments.layoutManager = LinearLayoutManager(requireActivity())
+
+        val adapter = ComentarioAdapter(comentarios)
+        binding.rvComments.adapter = adapter
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
     }
 
     companion object {
@@ -45,12 +91,12 @@ class ComentsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ComentsFragment.
+         * @return A new instance of fragment CommentsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ComentsFragment().apply {
+            CommentsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
