7ea7a1d1b73836b6191dfd00dda7b8c4b530e84e
Haroldleon21 <hellblazer47@gmail.com>
2021-12-09 03:26:44 -0500

    mapa

--- app/src/main/java/com/example/applogin/fragment_contactanos.kt
+++ app/src/main/java/com/example/applogin/fragment_contactanos.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +22,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [fragment_contactanos.newInstance] factory method to
  * create an instance of this fragment.
  */
-class fragment_contactanos : Fragment() {
+class fragment_contactanos : Fragment(), OnMapReadyCallback {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -55,5 +61,33 @@ class fragment_contactanos : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        var name = "Estampamos"
+        var address = "Bogotá, Cundinamarca"
+        var latitude = 4.6152032
+        var longitude = -74.0910327
+        var zoom = 15f
+
+        var centerMap = LatLng(latitude, longitude)
+        p0?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+
+
+        val marker = p0.addMarker(
+            MarkerOptions()
+                .title(name)
+                .position(centerMap)
+        )
+
+
     }
 }
