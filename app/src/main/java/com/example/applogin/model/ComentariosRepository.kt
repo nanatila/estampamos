7cc78fc8e193d20010ac9df143a96c02b4e4a924
nanatila <nanatila@gmail.com>
2021-12-10 09:02:58 -0500

    comentarios modificado

---
+++ app/src/main/java/com/example/applogin/model/ComentariosRepository.kt
@@ -0,0 +1,4 @@
+package com.example.applogin.model
+
+class ComentariosRepository {
+}
--- app/src/main/java/com/example/applogin/view/ui/fragments/fragment_contactanos.kt
+++ app/src/main/java/com/example/applogin/view/ui/fragments/fragment_contactanos.kt
@@ -1,10 +1,11 @@
-package com.example.applogin
+package com.example.applogin.view.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.applogin.R
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
